7088bd8661f325c907e5c90e3e55fa9c9bc362f7
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-04-21 09:46:05 +0200

    feat: show character on canvas

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,17 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 250;
     img;
+    height = 100;
+    width = 300;
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
 
     moveRight(){
-        
+        console.log('move right');
     }
 
     moveLeft(){
