256c25fe1342313a7238ec47d49837b12dc11815
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-04-25 16:57:16 +0200

    feat: keyboard connection feat: character movement

--- models/character.class.js
+++ models/character.class.js
@@ -1,34 +1,47 @@
 class Character extends MovableObject {
-    height = 300;
-    width = 150;
-    y = 135;
-    IMAGES_WALKING = [
-        'img/2_character_pepe/2_walk/W-21.png',
-        'img/2_character_pepe/2_walk/W-22.png',
-        'img/2_character_pepe/2_walk/W-23.png',
-        'img/2_character_pepe/2_walk/W-24.png',
-        'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png',
-    ];
+  height = 300;
+  width = 150;
+  y = 135;
+  speed = 5;
 
-    constructor() {
-        super();
-        this.loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages(this.IMAGES_WALKING);
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  world;
 
-        this.animate();
-    }
+  constructor() {
+    super();
+    this.loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
 
-    animate() {
-        setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 1000 / 10);
-    }
+    this.animate();
+  }
 
-    jump(){
+  animate() {
 
-    }
+    setInterval(() => {
+        if (this.world.keyboard.RIGHT) {
+             this.x += this.speed;
+        }   else if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        }
+    }, 1000 / 60);
+    
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 1000 / 10);
+  }
+
+  jump() {}
 }
--- models/world.class.js
+++ models/world.class.js
@@ -13,10 +13,18 @@ class World {
   ];
   canvas;
   ctx;
-  constructor(canvas) {
+  keyboard;
+
+  constructor(canvas, keyboard) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
+    this.keyboard = keyboard;
     this.draw();
+    this.setWorld();
+  }
+
+  setWorld() {
+    this.character.world = this;
   }
 
   draw() {
