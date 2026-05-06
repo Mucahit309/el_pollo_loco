72b535b1c4c2e0815308b47719a7084db6e0aabf
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-05-06 09:21:42 +0200

    feat: pepe idle animation

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,7 @@ class Character extends MovableObject {
   y = 135;
   speed = 5;
   collectedBottles = 0;
+  lastMoveTime = 0;
 
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
@@ -45,6 +46,31 @@ class Character extends MovableObject {
     "img/2_character_pepe/5_dead/D-56.png",
     "img/2_character_pepe/5_dead/D-57.png",
   ];
+  IMAGES_IDLE = [
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png",
+  ];
+  IMAGES_LONG_IDLE = [
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png",
+  ];
+
   world;
 
   constructor() {
@@ -54,20 +80,30 @@ class Character extends MovableObject {
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_HURT);
     this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_LONG_IDLE);
     this.applyGravity();
+    this.resetIdleTimer();
     this.animate();
+  }
+
+  resetIdleTimer() {
+    this.lastMoveTime = new Date().getTime();
   }
 
   animate() {
     setInterval(() => {
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.moveRight();
+        this.resetIdleTimer();
       } else if (this.world.keyboard.LEFT && this.x > 0) {
         this.moveLeft();
+        this.resetIdleTimer();
       }
 
       if (this.world.keyboard.SPACE && !this.isAboveGround()) {
         this.jump();
+        this.resetIdleTimer();
       }
 
       this.world.camera_x = -this.x + 100;
@@ -83,6 +119,13 @@ class Character extends MovableObject {
       } else {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
           this.playAnimation(this.IMAGES_WALKING);
+        } else {
+          let timePassed = new Date().getTime() - this.lastMoveTime;
+          if (timePassed >= 15000) {
+            this.playAnimation(this.IMAGES_LONG_IDLE);
+          } else {
+            this.playAnimation(this.IMAGES_IDLE);
+          }
         }
       }
     }, 1000 / 10);
